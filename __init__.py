6aa2a6cdf8735fe748e796a16b370019a568a415
mik.clucas@bluewin.ch <mik.clucas@bluewin.ch>
2020-05-18 08:01:04 +0200

    chg
    Signed-off-by: mik.clucas@bluewin.ch <mik.clucas@bluewin.ch>

--- __init__.py
+++ __init__.py
@@ -1,15 +1,27 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from adapt.intent import IntentBuilder
 
 
 class VideoCall(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        super().__init__()
 
     @intent_file_handler('call.video.intent')
     def handle_call_video(self, message):
-        self.speak_dialog('call.video')
+        person = message.data.get('person')
+        if person is not None:
+            if person == 'michael' or person == 'philip':
+                self.speak_dialog('call.video')
+            else:
+                self.speak_dialog('who.should.i.call', {'person': person})
+        else:
+            self.speak_dialog('who.should.i.call', {'person': "unknown"})
 
+def stop(self):
+    pass
 
 def create_skill():
     return VideoCall()
+
 
